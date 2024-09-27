9ff553ac5411fe2fcaa739a4644f0d0a950cfc98
Zephyr <wang_zi_mo@163.com>
2024-09-27 22:27:23 +0800

    Update foodieguide.sh

--- speedtest/sh/foodieguide.sh
+++ speedtest/sh/foodieguide.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 1. 搜索关键字，获取前5页结果
-echo "$(pwd)"
+# echo "$(pwd)"
+echo "yt-dlp 路径：$(which yt-dlp)"
 echo "==== 开始获取数据 ======"
 CHANNEL_NAME="台湾"
 CHANNEL_KEY_URL="%E5%8F%B0%E6%B9%BE"
@@ -77,48 +78,47 @@ grep -v '失败' "$SPEED_TEST_LOG" | sort -n -r | awk '{print $2 " " $1}' >valid
 besturl=$(head -n 1 validurl.txt | sed -n 's|.*//\([^/]*\)/.*|\1|p')
 echo "========== bestdomain : ${besturl}"
 
-# # 4. 获取 besturl 对应的直播源列表
+# 4. 获取 besturl 对应的直播源列表
 
-# echo "besturl 第1页 下载中"
-# curl -X POST "${URL}" \
-#     -H "Accept-Language: zh-CN,zh;q=0.9" \
-#     -d "search=${besturl}&Submit=+" \
-#     -c cookies.txt \
-#     -o "$BEST_URL_RESPONSE_FILE"
+echo "besturl 第1页 下载中"
+curl -X POST "${URL}" \
+    -H "Accept-Language: zh-CN,zh;q=0.9" \
+    -d "search=${besturl}&Submit=+" \
+    -c cookies.txt \
+    -o "$BEST_URL_RESPONSE_FILE"
 
-# max_page=$(grep -oP 'page=\K\d+' "$BEST_URL_RESPONSE_FILE" | sort -nr | head -n1)
-# l=$(grep -oP "&l=\K[^']*" "$BEST_URL_RESPONSE_FILE" | head -n 1)
-# echo "最大 page 值是: ${max_page}"
-# for page in $(seq 2 "$max_page"); do
-#     echo "besturl 第${page}页 下载中"
-#     curl -G "${URL}" \
-#         -H "Accept-Language: zh-CN,zh;q=0.9" \
-#         --data-urlencode "page=${page}" \
-#         --data-urlencode "s=${CHANNEL_KEY_URL}" \
-#         --data-urlencode "l=${l}" \
-#         -b cookies.txt \
-#         >>"$BEST_URL_RESPONSE_FILE"
-# done
-# rm cookies.txt
+max_page=$(grep -oP 'page=\K\d+' "$BEST_URL_RESPONSE_FILE" | sort -nr | head -n1)
+l=$(grep -oP "&l=\K[^']*" "$BEST_URL_RESPONSE_FILE" | head -n 1)
+echo "最大 page 值是: ${max_page}"
+for page in $(seq 2 "$max_page"); do
+    echo "besturl 第${page}页 下载中"
+    curl -G "${URL}" \
+        -H "Accept-Language: zh-CN,zh;q=0.9" \
+        --data-urlencode "page=${page}" \
+        --data-urlencode "s=${CHANNEL_KEY_URL}" \
+        --data-urlencode "l=${l}" \
+        -b cookies.txt \
+        >>"$BEST_URL_RESPONSE_FILE"
+done
+rm cookies.txt
 
-# # 使用 grep 和 awk 提取 <div style="float: left;"> 标签中的频道名称和 m3u8 链接
-# grep -oP '^\s*<div style="float: left;"[^>]*>\K[^<]*(?=</div>)|\s\Khttps[^<]*' "$BEST_URL_RESPONSE_FILE" |
-#     awk '{
-#     # 判断当前行是否包含 "http"
-#     if ($0 ~ /http/) {
-#         # 如果包含 "http"，则将其视为 URL
-#         gsub(/ /, "", $0);  # 去掉 URL 前后的空格
-#         if (channel != "") {  # 确保频道名称不为空
-#             print channel "," $0;  # 打印频道名称和对应的 URL
-#         }
-#     } else {
-#         # 如果不包含 "http"，则将其视为频道名称
-#         channel=$0;  # 保存频道名称
-#     }
-# }' >"$OUTPUT_FILE"
+# 使用 grep 和 awk 提取 <div style="float: left;"> 标签中的频道名称和 m3u8 链接
+grep -oP '^\s*<div style="float: left;"[^>]*>\K[^<]*(?=</div>)|\s\Khttps[^<]*' "$BEST_URL_RESPONSE_FILE" |
+    awk '{
+    # 判断当前行是否包含 "http"
+    if ($0 ~ /http/) {
+        # 如果包含 "http"，则将其视为 URL
+        gsub(/ /, "", $0);  # 去掉 URL 前后的空格
+        if (channel != "") {  # 确保频道名称不为空
+            print channel "," $0;  # 打印频道名称和对应的 URL
+        }
+    } else {
+        # 如果不包含 "http"，则将其视为频道名称
+        channel=$0;  # 保存频道名称
+    }
+}' >"$OUTPUT_FILE"
 
-# sed -i '' "1i ${CHANNEL_NAME},#genre#" "$OUTPUT_FILE"
-# echo " $OUTPUT_FILE 已经更新完成"
+sed -i '' "1i ${CHANNEL_NAME},#genre#" "$OUTPUT_FILE"
+echo " $OUTPUT_FILE 已经更新完成"
 
-# rm validurl.txt
-# rm ${RESPONSE_FILE} ${SEARCH_RESULTS_FILE} ${UNIQUE_SEARCH_RESULTS_FILE} ${SPEED_TEST_LOG} ${BEST_URL_RESPONSE_FILE}
+rm ${RESPONSE_FILE} ${SEARCH_RESULTS_FILE} ${UNIQUE_SEARCH_RESULTS_FILE} ${SPEED_TEST_LOG} ${BEST_URL_RESPONSE_FILE} validurl.txt
