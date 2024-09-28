304af540aeee57e17b1bed7ef1e6cec6c56dd08f
Zephyr <wang_zi_mo@163.com>
2024-09-28 11:42:29 +0800

    Update foodieguide.sh

--- foodieguide/foodieguide.sh
+++ foodieguide/foodieguide.sh
@@ -1,123 +1,119 @@
 #!/bin/bash
 
-# 1. 搜索关键字，获取前5页结果
-# echo "$(pwd)"
-echo "yt-dlp 路径：$(which yt-dlp)"
-echo "==== 开始获取数据 ======"
-CHANNEL_NAME="台湾"
-CHANNEL_KEY_URL="%E5%8F%B0%E6%B9%BE"
-CHANNEL_VALUE='eowuxJvaa8brWPsOa5vg=='
+# 定义城市参数
+declare -A cities
+cities["台湾"]="%E5%8F%B0%E6%B9%BE:eowuxJvaa8brWPsOa5vg=="
+cities["香港"]="%E9%A6%99%E6%B8%AF:eowuxJvaa8browuxowuxowuxea4rw=="
+cities["澳门"]="%E6%BE%B3%E9%97%A8:eowuxJvaa8braa8brsa8browuxXqA=="
 
 URL="http://foodieguide.com/iptvsearch/"
 RESPONSE_FILE="response.txt"
 UNIQUE_SEARCH_RESULTS_FILE="unique_searchresults.txt"
 SPEED_TEST_LOG="speedtest.log"
 BEST_URL_RESPONSE_FILE="besturlresponse.txt"
-OUTPUT_FILE="taiwan_foodieguide.txt"
 
-# 清空或创建响应文件
+# 清空或创建日志文件
 :>${SPEED_TEST_LOG}
 :>${UNIQUE_SEARCH_RESULTS_FILE}
-:>output.txt
-:>"${BEST_URL_RESPONSE_FILE}"
-# 获取数据
 
-echo "第1页 下载中"
-curl -X POST "${URL}" \
-    -H "Accept-Language: zh-CN,zh;q=0.9" \
-    -d "search=${CHANNEL_KEY_URL}&Submit=+" \
-    -c cookies.txt \
-    -o "$RESPONSE_FILE"
+for CHANNEL_NAME in "${!cities[@]}"; do
+    IFS=':' read -r CHANNEL_KEY_URL CHANNEL_VALUE <<< "${cities[$CHANNEL_NAME]}"
+    OUTPUT_FILE="${CHANNEL_NAME}_foodieguide.txt"
 
-for page in $(seq 2 3); do
-    echo "第${page}页 下载中"
-    curl -G "${URL}" \
+    echo "==== 开始获取数据: ${CHANNEL_NAME} ======"
+
+    # 清空响应文件
+    :>${RESPONSE_FILE}
+    # 获取数据
+    echo "第1页 下载中"
+    curl -X POST "${URL}" \
         -H "Accept-Language: zh-CN,zh;q=0.9" \
-        --data-urlencode "page=${page}" \
-        --data-urlencode "s=${CHANNEL_KEY_URL}" \
-        --data-urlencode "l=${CHANNEL_VALUE}" \
-        -b cookies.txt \
-        >>"$RESPONSE_FILE"
-done
-rm cookies.txt
+        -d "search=${CHANNEL_KEY_URL}&Submit=+" \
+        -c cookies.txt \
+        -o "$RESPONSE_FILE"
 
-# 2. 提取源地址，并进行整理
-# mac不支持-P参数，安装grep后使用ggrep
-grep -oP "\s\Khttps://[^<]*" "$RESPONSE_FILE" | awk -F/ '!seen[$3]++' >"$UNIQUE_SEARCH_RESULTS_FILE"
+    for page in $(seq 2 3); do
+        echo "第${page}页 下载中"
+        curl -G "${URL}" \
+            -H "Accept-Language: zh-CN,zh;q=0.9" \
+            --data-urlencode "page=${page}" \
+            --data-urlencode "s=${CHANNEL_KEY_URL}" \
+            --data-urlencode "l=${CHANNEL_VALUE}" \
+            -b cookies.txt \
+            >>"$RESPONSE_FILE"
+    done
+    rm cookies.txt
 
-# 3. 测速提取速度最好的源地址
-echo "==== 整理数据完成, 开始测速 ======"
-lines=$(wc -l <"$UNIQUE_SEARCH_RESULTS_FILE")
-i=0
-while read -r url; do
-    i=$((i + 1))
-    #    timeout 40
-    output=$(timeout 40 /usr/bin/yt-dlp --ignore-config --no-cache-dir --output "output.ts" --download-archive new-archive.txt --external-downloader ffmpeg --external-downloader-args "ffmpeg_i:-t 5" "${url}" 2>&1)
-    if echo "${output}" | grep -q "ERROR"; then
-        printf "第 %d/%d 个: 下载失败 %s\n" "$i" "$lines" ${url}
-        echo "下载失败：${url}" >>"$SPEED_TEST_LOG"
-        continue
-    fi
+    # 提取源地址，并进行整理
+    grep -oP "\s\Khttps://[^<]*" "$RESPONSE_FILE" | awk -F/ '!seen[$3]++' >"$UNIQUE_SEARCH_RESULTS_FILE"
 
-    speed=$(echo "${output}" | grep -B 1 -E '^video' | head -n 1 | grep -oP 'speed=\s?\K[0-9]+\.[0-9]+x\s+$' | sed 's| \+| |g')
-    # speed=$(echo "$output" | grep -oP 'at \K[0-9.]+[M|K]')
-    # speedinfo=$(echo "${output}" | grep -E '^size.*speed=' | head -n 1)
-    speedinfo=$(echo "${output}" | grep -B 1 -E '^video' | head -n 1)
+    # 测速提取速度最好的源地址
+    echo "==== 整理数据完成, 开始测速 ======"
+    lines=$(wc -l <"$UNIQUE_SEARCH_RESULTS_FILE")
+    i=0
+    while read -r url; do
+        i=$((i + 1))
+        output=$(timeout 40 /usr/bin/yt-dlp --ignore-config --no-cache-dir --output "output.ts" --download-archive new-archive.txt --external-downloader ffmpeg --external-downloader-args "ffmpeg_i:-t 5" "${url}" 2>&1)
+        if echo "${output}" | grep -q "ERROR"; then
+            printf "第 %d/%d 个: 下载失败 %s\n" "$i" "$lines" ${url}
+            echo "下载失败：${url}" >>"$SPEED_TEST_LOG"
+            continue
+        fi
 
-    echo "${output}" >>output.txt
-    rm -f new-archive.txt output.ts
+        speed=$(echo "${output}" | grep -B 1 -E '^video' | head -n 1 | grep -oP 'speed=\s?\K[0-9]+\.[0-9]+x\s+$' | sed 's| \+| |g')
+        speedinfo=$(echo "${output}" | grep -B 1 -E '^video' | head -n 1)
 
-    printf "第 %d/%d 个：%s\n[speedinfo]%s\n" "$i" "$lines" "$url" "$speedinfo"
-    echo "speed = ${speed} , ${url}"
+        echo "${output}" >>output.txt
+        rm -f new-archive.txt output.ts
 
-    echo "${speed} ${url}" >>"$SPEED_TEST_LOG"
-done <"$UNIQUE_SEARCH_RESULTS_FILE"
+        printf "第 %d/%d 个：%s\n[speedinfo]%s\n" "$i" "$lines" "$url" "$speedinfo"
+        echo "speed = ${speed} , ${url}"
 
-grep -v '失败' "$SPEED_TEST_LOG" | sort -n -r | awk '{print $2 " " $1}' >validurl.txt
-besturl=$(head -n 1 validurl.txt | sed -n 's|.*//\([^/]*\)/.*|\1|p')
-echo "========== bestdomain : ${besturl}"
+        echo "${speed} ${url}" >>"$SPEED_TEST_LOG"
+    done <"$UNIQUE_SEARCH_RESULTS_FILE"
 
-# 4. 获取 besturl 对应的直播源列表
+    grep -v '失败' "$SPEED_TEST_LOG" | sort -n -r | awk '{print $2 " " $1}' >validurl.txt
+    besturl=$(head -n 1 validurl.txt | sed -n 's|.*//\([^/]*\)/.*|\1|p')
+    echo "========== bestdomain : ${besturl}"
 
-echo "besturl 第1页 下载中"
-curl -X POST "${URL}" \
-    -H "Accept-Language: zh-CN,zh;q=0.9" \
-    -d "search=${besturl}&Submit=+" \
-    -c cookies.txt \
-    -o "$BEST_URL_RESPONSE_FILE"
-
-max_page=$(grep -oP 'page=\K\d+' "$BEST_URL_RESPONSE_FILE" | sort -nr | head -n1)
-l=$(grep -oP "&l=\K[^']*" "$BEST_URL_RESPONSE_FILE" | head -n 1)
-echo "最大 page 值是: ${max_page}"
-for page in $(seq 2 "$max_page"); do
-    echo "${besturl} 第${page}页 下载中"
-    curl -G "${URL}" \
+    # 获取 besturl 对应的直播源列表
+    echo "besturl 第1页 下载中"
+    curl -X POST "${URL}" \
         -H "Accept-Language: zh-CN,zh;q=0.9" \
-        --data-urlencode "page=${page}" \
-        --data-urlencode "s=${CHANNEL_KEY_URL}" \
-        --data-urlencode "l=${l}" \
-        -b cookies.txt \
-        >>"$BEST_URL_RESPONSE_FILE"
-done
-rm cookies.txt
+        -d "search=${besturl}&Submit=+" \
+        -c cookies.txt \
+        -o "$BEST_URL_RESPONSE_FILE"
 
-# 使用 grep 和 awk 提取 <div style="float: left;"> 标签中的频道名称和 m3u8 链接
-grep -oP '^\s*<div style="float: left;"[^>]*>\K[^<]*(?=</div>)|\s\Khttps[^<]*' "$BEST_URL_RESPONSE_FILE" |
-    awk '{
-    # 判断当前行是否包含 "http"
-    if ($0 ~ /http/) {
-        # 如果包含 "http"，则将其视为 URL
-        gsub(/ /, "", $0);  # 去掉 URL 前后的空格
-        if (channel != "") {  # 确保频道名称不为空
-            print channel "," $0;  # 打印频道名称和对应的 URL
+    max_page=$(grep -oP 'page=\K\d+' "$BEST_URL_RESPONSE_FILE" | sort -nr | head -n1)
+    l=$(grep -oP "&l=\K[^']*" "$BEST_URL_RESPONSE_FILE" | head -n 1)
+    echo "最大 page 值是: ${max_page}"
+    for page in $(seq 2 "$max_page"); do
+        echo "${besturl} 第${page}页 下载中"
+        curl -G "${URL}" \
+            -H "Accept-Language: zh-CN,zh;q=0.9" \
+            --data-urlencode "page=${page}" \
+            --data-urlencode "s=${CHANNEL_KEY_URL}" \
+            --data-urlencode "l=${l}" \
+            -b cookies.txt \
+            >>"$BEST_URL_RESPONSE_FILE"
+    done
+    rm cookies.txt
+
+    # 提取频道名称和 m3u8 链接
+    grep -oP '^\s*<div style="float: left;"[^>]*>\K[^<]*(?=</div>)|\s\Khttps[^<]*' "$BEST_URL_RESPONSE_FILE" |
+        awk '{
+        if ($0 ~ /http/) {
+            gsub(/ /, "", $0);
+            if (channel != "") {
+                print channel "," $0;
+            }
+        } else {
+            channel=$0;
         }
-    } else {
-        # 如果不包含 "http"，则将其视为频道名称
-        channel=$0;  # 保存频道名称
-    }
-}' >"$OUTPUT_FILE"
+    }' >"$OUTPUT_FILE"
 
-sed -i "1i ${CHANNEL_NAME},#genre#" "$OUTPUT_FILE"
-echo " $OUTPUT_FILE 已经更新完成"
+    sed -i "1i ${CHANNEL_NAME},#genre#" "$OUTPUT_FILE"
+    echo " $OUTPUT_FILE 已经更新完成"
 
-# rm ${RESPONSE_FILE} ${UNIQUE_SEARCH_RESULTS_FILE} ${SPEED_TEST_LOG} ${BEST_URL_RESPONSE_FILE} validurl.txt
+    rm ${RESPONSE_FILE} ${UNIQUE_SEARCH_RESULTS_FILE} ${SPEED_TEST_LOG} ${BEST_URL_RESPONSE_FILE} validurl.txt
+done
