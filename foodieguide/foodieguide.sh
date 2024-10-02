24c22c70ce53573ea5e31bdb74e38064fff21fe9
Zephyr <wang_zi_mo@163.com>
2024-10-02 17:01:05 +0800

    Update foodieguide.sh

--- foodieguide/foodieguide.sh
+++ foodieguide/foodieguide.sh
@@ -3,8 +3,8 @@
 # 定义城市参数
 declare -A cities
 cities["taiwan"]="%E5%8F%B0%E6%B9%BE:eowuxJvaa8brWPsOa5vg=="
-cities["hongkong"]="%E9%A6%99%E6%B8%AF:eowuxJvaa8browuxowuxowuxea4rw=="
-cities["macao"]="%E6%BE%B3%E9%97%A8:eowuxJvaa8braa8brsa8browuxXqA=="
+# cities["hongkong"]="%E9%A6%99%E6%B8%AF:eowuxJvaa8browuxowuxowuxea4rw=="
+# cities["macao"]="%E6%BE%B3%E9%97%A8:eowuxJvaa8braa8brsa8browuxXqA=="
 
 URL="http://foodieguide.com/iptvsearch/"
 RESPONSE_FILE="response.txt"
