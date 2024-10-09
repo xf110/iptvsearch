9032255ad38b9144109450819deb3f33c4b43f28
Zephyr <wang_zi_mo@163.com>
2024-10-09 19:59:17 +0800

    Update fofa2.sh

--- speedtest/fofa2.sh
+++ speedtest/fofa2.sh
@@ -12,6 +12,9 @@ cities=(
     [2]="BeiJing_unicom_145 rtp/239.3.1.236:2000 北京联通 'udpxy && country=\"CN\" && region=\"Beijing\" && org=\"China Unicom Beijing Province Network\" && protocol=\"http\"'"
     [3]="SiChuan_unicom_334 udp/239.93.0.169:5140 四川联通 'udpxy && country=\"CN\" && region=\"Sichuan\" && org=\"CHINA UNICOM China169 Backbone\" && protocol=\"http\"'"
     [5]="HeNan_unicom_172 rtp/225.1.4.98:1127 河南联通 'udpxy && country=\"CN\" && region=\"HeNan\" && org=\"CHINA UNICOM China169 Backbone\" && protocol=\"http\"'"
+    [6]="ChongQing_telecom_161 rtp/235.254.196.249:1268 重庆电信 'udpxy && country=\"CN\" && region=\"Chongqing\" && org=\"Chinanet\" && protocol=\"http\"'"
+    [7]="FuJian_telecom_114 rtp/239.61.3.4:9542 附件电信 'udpxy && country=\"CN\" && region=\"Fujian\" && org=\"Chinanet\" && protocol=\"http\"'"
+
 )
 
 # 定义处理城市的函数
