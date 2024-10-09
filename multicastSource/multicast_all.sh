f72856c796a1d9ed7cf9d9d5b781ef2c7870cc22
Zephyr <wang_zi_mo@163.com>
2024-10-09 17:40:35 +0800

    Update multicast_all.sh

--- multicastSource/multicast_all.sh
+++ multicastSource/multicast_all.sh
@@ -344,9 +344,9 @@ for CHANNEL_NAME in "${!cities[@]}"; do
 
     sed -i "1i ${CHANNEL_NAME},#genre#" "$OUTPUT_FILE"
     line_count_output=$(($(wc -l < "$OUTPUT_FILE") - 1))
-    NEW_output="${OUTPUT_FILE//NUM/$line_count_output}"
+    NEW_output="${OUTPUT_FILE//NUM/$line_count_output}
     mv "$OUTPUT_FILE" "$NEW_output"
-    echo " $$NEW_output 已经更新完成" | tee -a "$SUMMARY_FILE"
+    echo " $NEW_output 已经更新完成" | tee -a "$SUMMARY_FILE"
 
     # 在汇总文件中加入分隔行
     echo "==== ${CHANNEL_NAME} 处理完成 ======" | tee -a "$SUMMARY_FILE"
