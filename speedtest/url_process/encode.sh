6480665e8daa3a2465788a51ef26436539292fee
Zephyr <wang_zi_mo@163.com>
2024-10-11 12:03:38 +0800

    Create encode.sh

---
+++ speedtest/url_process/encode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 使用命令行传递参数： urlencode.sh "fqw000 url_process"
+
+urlencode() {
+    # urlencode <string>
+
+    old_lang=$LANG
+    LANG=C
+    
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LANG=$old_lang
+    LC_COLLATE=$old_lc_collate
+}
+
+urlencode "$1"
