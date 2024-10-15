daaf573b35f570ab3ad123733ae42690a6386da0
Zephyr <wang_zi_mo@163.com>
2024-10-15 22:25:31 +0800

    Update gangaotai_gat.sh

--- gangaotai/gangaotai_gat.sh
+++ gangaotai/gangaotai_gat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set -e  # 遇到错误时立即退出
-set -x # 调试
+# set -x # 调试
 
 # 定义城市参数
 declare -A cities
@@ -194,7 +194,7 @@ for channel_name in "${!cities[@]}"; do
 
     echo "${channel_name}_$linescount ：${best_url}" >> msg.tmp
 
-    rm ${response_file} ${unique_search_results_file} ${speed_test_log} ${best_url_response_file} ${yt_dlp_log} out.tmp valid_url.txt
+    rm ${response_file} ${unique_search_results_file} ${speed_test_log} ${best_url_response_file} ${yt_dlp_log} $summary_file out.tmp valid_url.txt
 
 done
 sed -i "1i $(TZ='Asia/Shanghai' date +%Y/%m/%d/%H:%M:%S)\n港澳台直播源列表已更新: " msg.tmp
