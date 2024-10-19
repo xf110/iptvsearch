1caf13bcc4d7edd0b6c04899d35308ec17d5744f
Zephyr <wang_zi_mo@163.com>
2024-10-20 01:21:38 +0800

    Update gangaotai_gat.sh

--- gangaotai/gangaotai_gat.sh
+++ gangaotai/gangaotai_gat.sh
@@ -5,7 +5,7 @@
 
 # 定义城市参数
 declare -A cities
-cities["taiwan"]="%E5%8F%B0%E6%B9%BE:eowuxJvaa8brWPsOa5vg=="
+cities["taiwan"]="%E5%8F%B0%E6%B9%B%E6%B0%91%E8%A7%86:eowuxJvaa8brWPsOa5vg=="
 cities["hongkong"]="%E9%A6%99%E6%B8%AF:eowuxJvaa8browuxowuxowuxea4rw=="
 cities["macau"]="%E6%BE%B3%E9%97%A8:eowuxJvaa8braa8brsa8browuxXqA=="
 cities["feicuitai"]="%E7%BF%A1%E7%BF%A0%E5%8F%Bl:eowuxJvaa8bre/oee/oOWPsA=="
